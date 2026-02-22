57f54af81b8210070d6a00833afc698e229458dd
Rahul Mishra <rm9193421@gmail.com>
2026-02-22 15:26:49 +0530

    Refactor Navbar to add title and remove logo
    Updated the Navbar component to include a clickable title and removed the logo image.

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -10,7 +10,21 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3'>
       <Link to='/'>
-      <img src={assets.logo} alt="Logo" className='w-28 lg:w-32'/>
+      {/* <img src={assets.logo} alt="Logo" className='w-28 lg:w-32'/> */}
+        <div
+        onClick={() => navigate("/")}
+        className="cursor-pointer flex flex-col leading-tight"
+      >
+        <span className="text-blue-700 font-extrabold text-sm sm:text-base lg:text-lg tracking-wide uppercase">
+          Intelligent E-Learning
+        </span>
+        <span className="text-cyan-600 font-semibold text-xs sm:text-sm lg:text-base tracking-widest uppercase">
+          Management System
+        </span>
+        <span className="text-gray-400 font-medium text-xs tracking-widest uppercase">
+          (LMS)
+        </span>
+      </div>
       </Link>
       <div className='flex items-center gap-5 text-gray-500 relative'>
         <p>Hi! {user ? user.fullName : 'Developers'}</p>
